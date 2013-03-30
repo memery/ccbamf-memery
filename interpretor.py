8b5c21c000734d1acca24e261ff6ed8468c191ab
nycz <pyntix@gmail.com>
2013-03-30 12:31:16 +0100

    Wrote plugin functionality for interpretor
    Added primes as a placeholder testplugin.

--- interpretor.py
+++ interpretor.py
@@ -1,5 +1,10 @@
-import threading
+import imp
+import os
+import os.path
 import queue
+import sys
+import threading
+import time
 
 from common import Actor
 
@@ -9,7 +14,7 @@ class InterpretorActor(Actor):
 
     def initialize(self):
         self.wait_for_message = False
-        self.active_processes = []
+        self.active_threads = []
 
     def main_loop(self, message):
         if message:
@@ -18,41 +23,79 @@ class InterpretorActor(Actor):
                 self.stop()
                 return
             elif subject == 'interpret':
-                destination, author, content = payload
-                q = queue.Queue()
-                worker = ParserWorker(source, destination, author, content, q)
-                self.active_processes.append((q, worker))
+                self.spawn_worker_swarm(source, *payload)
 
-        for item in list(self.active_processes):
-            q, process = item
+        for item in list(self.active_threads):
+            q, thread, birthdate = item
             try:
                 data = q.get_nowait()
             except queue.Empty:
                 pass
             else:
-                target, destination, response = data
-                self.send(target, 'response', (destination, response))
-            if not process.is_alive():
-                self.active_processes.remove(item)
+                subject, content = data
+                if subject == 'response':
+                    target, destination, response = content
+                    self.send(target, 'response', (destination, response))
+                elif subject == 'error':
+                    self.send('logger:error', None, content)
+            if not thread.is_alive():
+                self.active_threads.remove(item)
+
+    def spawn_worker_swarm(self, source, destination, author, content):
+        for plugin_data in list_plugins():
+            q = queue.Queue()
+            timestamp = time.time()
+            worker = PluginExecutor(plugin_data, source, destination, author, content, q)
+            self.active_threads.append((q, worker, timestamp))
+            worker.start()
 
 
-class ParserWorker(threading.Thread):
-    def __init__(self, source, destination, author, content, response_queue):
+
+class PluginExecutor(threading.Thread):
+    def __init__(self, plugin_data, source, destination, author, content, response_queue):
         super().__init__()
+        self.plugin_data = plugin_data
         self.source = source
         self.destination = destination
         self.author = author
         self.content = content
         self.response_queue = response_queue
-        self.start()
 
     def run(self):
-        pairs = {
-            '.ping': 'pong',
-            'hai': 'hay'
-        }
-        if self.content in pairs:
-            out = (self.source, self.destination,
-                   self.author + ': ' + pairs[self.content])
-            self.response_queue.put(out)
+        try:
+            loaded_plugin = load_plugin(self.plugin_data)
+        except ImportError:
+            self.respond('error', 'Could not import {}'.format(self.plugin_data))
+            return
+        response = loaded_plugin.run(self.author, self.content)
+        if response:
+            self.respond('response', (self.source, self.destination, response))
 
+    def respond(self, subject, message):
+        self.response_queue.put((subject, message))
+
+
+def list_plugins():
+    root_path = os.path.join(sys.path[0], 'plugins')
+    plugin_dirs = [os.path.join(root_path,d)
+                   for d in os.listdir(root_path)
+                   if os.path.isdir(os.path.join(root_path,d))]
+    plugins = [(os.path.splitext(f)[0], os.path.join('plugins', subdir))
+               for subdir in plugin_dirs for f in os.listdir(subdir)
+               if os.path.splitext(f)[1] == '.py']
+    return plugins
+
+def load_plugin(plugin_data):
+    plugin_name, plugin_path = plugin_data
+    try:
+        fp, pathname, description = imp.find_module(plugin_name, [plugin_path])
+        plugin = imp.load_module(plugin_name, fp, pathname, description)
+    except Exception as e:
+        raise e
+    else:
+        return plugin
+    finally:
+        try:
+            fp.close()
+        except UnboundLocalError:
+            pass
---
+++ plugins/built-in/primes.py
@@ -0,0 +1,33 @@
+
+def help():
+    return {'authors':     ['kqr'],
+            'years':       ['2012'],
+            'version':     '0.8',
+            'description': 'Calculates prime factors.',
+            'argument':    '<natural number n, where 2 < n < 100000>'}
+
+def run(nick, args):
+    if not (args.startswith('.primes ') and args.lstrip('.primes ').isdigit()):
+        return None
+    args = args.lstrip('.primes ')
+    try:
+        number = int(args)
+        if number <= 2 or number > 99999:
+            raise ValueError
+    except ValueError:
+        return "Argument must be an integer between 2 and not too high!"
+
+    return "{}: {} = {}".format(nick, number, ' * '.join([str(f) for f in primes(number)]))
+
+def primes(number):
+    factors = []
+    factor = 0
+
+    while factor < number:
+        factor = 2
+        while number % factor != 0:
+            factor += 1
+        number /= factor
+        yield factor
+
+
